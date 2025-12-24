81cc543fd4f425006a11b5ac281f63d0c9b233e0
konsowa202 <mahmoudkonsowa3030@gmail.com>
2025-12-24 20:29:51 +0200

    not hash commented-out closing brace

--- app/meet/[id]/page.tsx
+++ app/meet/[id]/page.tsx
@@ -1,4 +1,4 @@
-/* "use client"
+ "use client"
 
 import { useState, useEffect } from "react"
 import { useParams, useRouter } from "next/navigation"
@@ -103,4 +103,4 @@ export default function MeetingPage() {
             </div>
         </div>
     )
-} */
+}
