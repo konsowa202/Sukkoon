728bd4a52902eedaa354be4d04918d6b3020509c
konsowa202 <mahmoudkonsowa3030@gmail.com>
2025-12-24 20:12:33 +0200

    Comment out client directive in page.tsx

--- app/meet/[id]/page.tsx
+++ app/meet/[id]/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+/* "use client"
 
 import { useState, useEffect } from "react"
 import { useParams, useRouter } from "next/navigation"
@@ -104,3 +104,4 @@ export default function MeetingPage() {
         </div>
     )
 }
+*/
