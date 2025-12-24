b50d09aeaf8cda2844a5b74f0bed1aa6726fc9e6
konsowa202 <mahmoudkonsowa3030@gmail.com>
2025-12-24 20:13:38 +0200

    Remove client directive and unused code

--- app/meet/[id]/page.tsx
+++ app/meet/[id]/page.tsx
@@ -1,4 +1,4 @@
-/* "use client"
+ "use client"
 
 import { useState, useEffect } from "react"
 import { useParams, useRouter } from "next/navigation"
@@ -104,4 +104,3 @@ export default function MeetingPage() {
         </div>
     )
 }
-*/
